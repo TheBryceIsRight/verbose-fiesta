7bd63dc0f7b85a15b35b296a7606c5774ed0538f
Bryce Watson <brycewatson315@gmail.com>
2023-02-11 19:06:54 -0500

    added system status

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} color="secondary">
             Bryce's Portfolio
           </Typography>
-          <Link href="/work/functionize">
+          <Link href="/work/elavon-status">
             <IconButton
               size="large"
               edge="start"
