3724adf2f0447ad23c0f51119f3341e2f7004ef0
Bryce Watson <brycewatson315@gmail.com>
2023-02-14 22:18:31 -0500

    more meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,24 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <meta charSet="utf-8"></meta>
-        <meta property="og:type" content="blog"></meta>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
-        <meta property="twitter:image:alt" content="laptop with user interface and the AllVoices logo"></meta>
-        <meta property="twitter:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
-        <meta property="og:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta property="twitter:title" content="Bryce Watson's Design Portfolio"></meta>
-        <meta property="twitter:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
-        <meta property="og:image:type" content="image/png"></meta>
-        <meta property="og:title" content="Bryce Watson's Design Portfolio"></meta>
-        <meta property="og:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
-        <meta property="og:image:width" content="1446"></meta>
-        <meta property="og:site_name" content="Bryce Watson's Design Portfolio"></meta>
-        <meta property="og:image:height" content="1024"></meta>
-        <meta property="article:author" content="Bryce Watson"></meta>
-        <meta property="og:url" content="https://bryce-watson-design.vercel.app/"></meta>
+
       </Head>
       <link
         rel="stylesheet"
