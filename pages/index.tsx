cfe9e325ae8bd336b736a07d4bf736fc3f280b5b
Bryce Watson <brycewatson315@gmail.com>
2023-02-15 13:57:18 -0500

    updated types

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,6 @@ export default function Index({ allPosts }: Props) {
             <HeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
               slug={heroPost.slug}
               excerpt={heroPost.excerpt}
             />
