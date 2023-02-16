fb46046f0b6f0b0d6111b957f753fb8255f0c8f7
Bryce Watson <brycewatson315@gmail.com>
2023-02-16 13:08:48 -0500

    meta tags

--- components/meta.tsx
+++ components/meta.tsx
@@ -36,20 +36,20 @@ const Meta = () => {
         <meta property="og:type" content="blog"></meta>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
         <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
-        <meta property="twitter:image:alt" content="laptop with user interface and the AllVoices logo"></meta>
-        <meta property="twitter:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
-        <meta property="og:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1672785472/AllVoices_pyvu6b.png"></meta>
+        <meta property="twitter:image:alt" content="Hogsmeade by Sandra Benito, graphite"></meta>
+        <meta property="twitter:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1676570779/hogsmeade_wnyi3p.jpg"></meta>
+        <meta property="og:image" content="https://res.cloudinary.com/bryce-portfolio/image/upload/v1676570779/hogsmeade_wnyi3p.jpg"></meta>
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta property="twitter:title" content="Bryce Watson's Design Portfolio"></meta>
-        <meta property="twitter:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
+        <meta property="twitter:title" content="Sandra Benito's Design Portfolio"></meta>
+        <meta property="twitter:description" content="My portfolio"></meta>
         <meta property="og:image:type" content="image/png"></meta>
-        <meta property="og:title" content="Bryce Watson's Design Portfolio"></meta>
-        <meta property="og:description" content="A history of my work as a User Experience Designer from Georgia Tech until now"></meta>
+        <meta property="og:title" content="Sandra Benito's Design Portfolio"></meta>
+        <meta property="og:description" content="My portfolio"></meta>
         <meta property="og:image:width" content="1446"></meta>
-        <meta property="og:site_name" content="Bryce Watson's Design Portfolio"></meta>
+        <meta property="og:site_name" content="Sandra Benito's Design Portfolio"></meta>
         <meta property="og:image:height" content="1024"></meta>
-        <meta property="article:author" content="Bryce Watson"></meta>
-        <meta property="og:url" content="https://bryce-watson-design.vercel.app/"></meta>
+        <meta property="article:author" content="Sandra Benito Junyent"></meta>
+        <meta property="og:url" content="https://sandra-benito-portfolio.vercel.app/"></meta>
     </Head>
   )
 }
